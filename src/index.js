d1ab8f3c0a85b992361ca528ba5e9f51c27798b2
Rahmonbek <83601261+Rahmonbek@users.noreply.github.com>
2022-07-13 15:22:03 +0500

    bdfbfdb

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-import 'react-app-polyfill/stable'
-import 'core-js'
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-import { Provider } from 'react-redux'
-import store from './store'
+/*!
+
+=========================================================
+* Vision UI Free Chakra - v1.0.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/vision-ui-free-chakra
+* Copyright 2021 Creative Tim (https://www.creative-tim.com/)
+* Licensed under MIT (https://github.com/creativetimofficial/vision-ui-free-chakra/blob/master LICENSE.md)
+
+* Design and Coded by Simmmple & Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
+
+import AuthLayout from "layouts/Auth.js";
+import AdminLayout from "layouts/Admin.js";
+import RTLLayout from "layouts/RTL.js";
 
 ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root'),
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+  <HashRouter>
+    <Switch>
+      <Route path={`/auth`} component={AuthLayout} />
+      <Route path={`/admin`} component={AdminLayout} />
+      <Route path={`/rtl`} component={RTLLayout} />
+      <Redirect from={`/`} to='/admin/dashboard' />
+    </Switch>
+  </HashRouter>,
+  document.getElementById("root")
+);
